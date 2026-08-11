7f9868a454bfc0d0a2445521a7a5b1ff11747c5e
dattruong88vn <dattruong88vn@gmail.com>
2026-08-11 13:13:45 +0700

    [config] update env for production domain

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,20 +4,21 @@ const WebpackModuleFederation = require('webpack/lib/container/ModuleFederationP
 const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const DOMAIN = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: "production",
     output: {
         filename: '[name].[contenthash].js',
         // production: sync code to S3 in folder: /container/latest
+        // call between files in container
         publicPath: '/container/latest/'
     },
     plugins: [
         new WebpackModuleFederation({
             name: 'container',
             remotes: {
-                marketing: `marketing@marketing/latest/remoteEntry.js`
+                marketing: `marketing@${DOMAIN}/marketing/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -8,7 +8,8 @@ const commonConfig = require("./webpack.common");
 const prodConfig = {
     mode: "production",
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/' // call between files in marketing
     },
     plugins: [
         new WebpackModuleFederation({
