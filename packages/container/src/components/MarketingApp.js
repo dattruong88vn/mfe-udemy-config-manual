d684e7a0a8a9484674fc79663c61851208df0faf
dattruong88vn <dattruong88vn@gmail.com>
2026-08-13 14:38:17 +0700

    [feature] communite between router history

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,18 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    // mount fn import from child
+    // pass params to child: ref -> build DOM, onNavigate: sync history from child to parent
+    // mount fn return onParentNavigate, use to sync history from parent to child
+    const { onParentNavigate } = mount(ref.current, { onNavigate: ({ pathname: nextPathName }) => { const { pathname } = history.location; if (pathname !== nextPathName) { history.push(nextPathName); } } });
+
+    history.listen(onParentNavigate)
   });
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from "history" // phải đổi lên react-router version cao hơn, sử dụng function của thư viện ko phải qua history
 
 import App from "./App"
 
 // mount fn
-const mount = (el) => {
-    ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        // execute callback each time navigate
+        // pass location data back to Container
+        history.listen((location) => onNavigate(location))
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    // return function that container trigger each time navigate
+    return {
+        onParentNavigate: ({ pathname: nextParentPathname }) => {
+            const pathname = history.location;
+
+            if (pathname !== nextParentPathname) {
+                history.push(nextParentPathname)
+            }
+        }
+    }
 }
 
 // call immediately in development mode
 if (process.env.NODE_ENV === 'development') {
     const el = document.querySelector('#my-marketing-root');
-    if (el) mount(el)
+    if (el) mount(el, { defaultHistory: createBrowserHistory() })
 }
 
 // integrate with container
